d754c18d519b360c803d0a64141dcc8a9c508b3f
RAZYANIK <mdfarhanrazy@gmail.com>
2022-09-30 08:18:47 +0600

    4th commit

--- src/Component/LandingPage/LandingPage.js
+++ src/Component/LandingPage/LandingPage.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SidePanel from '../SidePanel/SidePanel';
+import Task from '../Task/Task';
+
 import './LandingPage.css'
 const LandingPage = () => {
+    const [tasks, setTasks] = useState([]);
+    const [panel, setPanel] = useState([]);
+
+    useEffect(() => {
+        fetch('fakedata.json')
+            .then(res => res.json())
+            .then(data => setTasks(data));
+    }, []);
+
+    const handleAssignTask = (task) => {
+        // console.log("clicked",task);
+        const newPanel = [...panel, task];
+        setPanel(newPanel);
+    }
+
     return (
         <div>
+            <div className='header'>
+                <h1>Daily Exercises Tracker</h1>
+                <h3 >Select Your Today's Exercise</h3>
+            </div>
+            <div className='dashboard-container'>
+                <div className="tasks-container">
+                    {
+                        tasks.map(task => <Task
+                            key={task.id}
+                            task={task}
+                            handleAssignTask={handleAssignTask}
+                        ></Task>)
+                    }
+                </div>
+                <div className="panel-container">
+                    <SidePanel>
+                        panel={panel}
+                    </SidePanel>
+                </div>
+            </div>
 
 
         </div>
--- src/Component/Task/Task.js
+++ src/Component/Task/Task.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './Task.css'
-const Task = () => {
+const Task = (props) => {
+    const { handleAssignTask, task } = props;
+    const { picture, taskName, timeRequired } = task;
     return (
-        <div>
+        <div className='task'>
+            <img src={picture} alt="" />
+            <div>
+                <p className='task-name'>{taskName}</p>
+                <p className='task-time'> Allocated Time: {timeRequired} Minutes</p>
+            </div>
+            <button onClick={() => handleAssignTask(props.task)} className='btn-task-assign'>
+                <p>Add to Exercise</p>
+            </button>
 
         </div>
     );
